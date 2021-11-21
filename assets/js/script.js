6b7abdb16950e06e446f920bbc09cb54ddcc3b03
marinamarshall <marinamarshall2@gmail.com>
2021-11-21 15:34:33 +0000

    Find bug in JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 // My API key
 const API_KEY = "RewmKb--0SB7E0QTShjvTjX9eEs";
-
 // Testing that the key works for GET method
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+
 async function getStatus(e) {
     // Make get request to API_URL with the API_KEY
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
     // Pass this data to a function that will display it
-    const queryString = `$(API_URL)?api_key=$(API_KEY)`;
-
     const response = await fetch(queryString);
 
     const data = await response.json();
@@ -20,4 +18,7 @@ async function getStatus(e) {
     if (response.ok) {
         console.log(data.expiry);
     }
+
 }
+
+
