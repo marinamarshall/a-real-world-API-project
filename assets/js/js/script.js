9eee62c1a23d98ec96b13d78c4e698573f164bd9
marinamarshall <marinamarshall2@gmail.com>
2021-11-21 14:50:00 +0000

    Add constants and GET request

--- assets/js/js/script.js
+++ assets/js/js/script.js
@@ -1,5 +1,23 @@
 // My API key
-RewmKb--0SB7E0QTShjvTjX9eEs
+const API_KEY = "RewmKb--0SB7E0QTShjvTjX9eEs";
 
 // Testing that the key works for GET method
-https://ci-jshint.herokuapp.com/api?api_key=RewmKb--0SB7E0QTShjvTjX9eEs
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    // Make get request to API_URL with the API_KEY
+    // Pass this data to a function that will display it
+    const queryString = `$(API_URL)?api_key=$(API_KEY)`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
